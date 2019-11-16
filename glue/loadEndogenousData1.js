a072e11b1f1556c9ccfc5a8e15ce28b39594c109
Rob Gifford <robjgiff@gmail.com>
2019-11-16 17:51:47 -0300

    Added new importers, misc mods to build

--- glue/loadEndogenousData1.js
+++ glue/loadEndogenousData1.js
@@ -1,5 +1,5 @@
 // list the circovirus sequences
-var listSeqResult = glue.command(["list", "sequence", "-w", "source.name = 'fasta-digs'"]);
+var listSeqResult = glue.command(["list", "sequence", "-w", "source.name = 'fasta-curated'"]);
 // extract from the result a list of sequence IDs.
 var seqIds = glue.getTableColumn(listSeqResult, "sequenceID");
 
@@ -8,7 +8,7 @@ _.each(seqIds, function(seqId) {
     // create an object in the custom table which uses the sequence ID as the row ID.
     glue.command(["create", "custom-table-row", "endogenous_data", seqId]);
     // associate the corresponding sequence with this object.
-    glue.inMode("sequence/fasta-digs/"+seqId, function() {
+    glue.inMode("sequence/fasta-curated/"+seqId, function() {
         glue.command(["set", "link-target", "endogenous_data", "custom-table-row/endogenous_data/"+seqId]);
     });
 });
