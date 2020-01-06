e294bed28875bfbfb46d5cb4cabc6e603c3765b8
Rob Gifford <robjgiff@gmail.com>
2020-01-06 16:31:40 +0000

    Updated to new project schema

--- glue/eve/loadLocusData2.js
+++ glue/eve/loadLocusData2.js
@@ -1,7 +1,7 @@
 // Load EVE data from tab file 
 var loadResult;
 glue.inMode("module/circoviridaeTabularUtility", function() {
-	loadResult = glue.tableToObjects(glue.command(["load-tabular", "tabular/ecv-side-data.tsv"]));
+	loadResult = glue.tableToObjects(glue.command(["load-tabular", "tabular/eve/ecv-side-data.tsv"]));
 	// glue.log("INFO", "load result was:", loadResult);
 });
 
@@ -9,7 +9,7 @@ _.each(loadResult, function(eveObj) {
 
 	// glue.log("INFO", "eveObj was:", eveObj);
 
-	glue.inMode("custom-table-row/endogenous_data/"+eveObj.id, function() {
+	glue.inMode("custom-table-row/locus_data/"+eveObj.id, function() {
 	
 		glue.command(["set", "field", "scaffold", eveObj.scaffold]);
 		glue.command(["set", "field", "start_position", eveObj.extract_start]);
