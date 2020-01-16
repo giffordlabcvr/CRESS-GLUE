e7097be9203692b831926345642150f6637ebc95
Rob Gifford <robjgiff@gmail.com>
2020-01-16 22:27:57 +0000

    Validating alignment build and linking with locus data

--- glue/eve/loadLocusData2.js
+++ glue/eve/loadLocusData2.js
@@ -7,7 +7,7 @@ glue.inMode("module/cressTabularUtility", function() {
 
 _.each(loadResult, function(eveObj) {
 
-	// glue.log("INFO", "eveObj was:", eveObj);
+    glue.log("INFO", "eveObj was:", eveObj);
 
 	glue.inMode("custom-table-row/locus_data/"+eveObj.id, function() {
 	
@@ -15,6 +15,16 @@ _.each(loadResult, function(eveObj) {
 		glue.command(["set", "field", "start_position", eveObj.extract_start]);
 		glue.command(["set", "field", "end_position", eveObj.extract_end]);
 		glue.command(["set", "field", "orientation", eveObj.orientation]);
-		glue.command(["set", "field", "host_sci_name", eveObj.orientation]);
+		glue.command(["set", "field", "host_sci_name", eveObj.host_sci_name]);
 	});
+
+	glue.inMode("sequence/fasta-curated-ecv/"+eveObj.id, function() {
+	
+		glue.command(["set", "field", "name", eveObj.virus_name]);
+		glue.command(["set", "field", "full_name", eveObj.full_name]);
+		glue.command(["set", "field", "family", eveObj.virus_family]);
+		glue.command(["set", "field", "clade", eveObj.virus_clade]);
+		glue.command(["set", "field", "genus", eveObj.virus_genus]);
+	});
+
 });
