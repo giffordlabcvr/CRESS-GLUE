4fabe962938c035402cbabba4028f49165f6e8d8
Rob Gifford <robjgiff@gmail.com>
2020-01-16 13:03:44 +0000

    Streamlining build

--- glue/core/cressInheritFeatureLocations.js
+++ glue/core/cressInheritFeatureLocations.js
@@ -1,5 +1,5 @@
 //list of parent features which should be transferred - the parent of the list elements is 'whole_genome'
-var nonCodingFeaturesToInherit = ["Cap", "Cap-Rev"];
+var nonCodingFeaturesToInherit = ["Rep", "Cap"];
 
 
 //initially remove all feature location annotations from the reference sequences with the exception of the master
