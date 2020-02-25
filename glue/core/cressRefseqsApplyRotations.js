dcaac8e9b6ef01b0a1a0da7ff88ce6192f13dd18
Rob Gifford <robjgiff@gmail.com>
2020-02-25 15:46:36 +0000

    Updated alignments

--- glue/core/cressRefseqsApplyRotations.js
+++ glue/core/cressRefseqsApplyRotations.js
@@ -4,9 +4,7 @@ var whereClause = "source.name in ('ncbi-refseqs')";
 
 glue.command(["multi-unset", "field", "sequence", "-w", whereClause, "rotation"]);
 
-
 shiftLeft("ncbi-refseqs/NC_026639", 2045);
-
 
 function shiftLeft(refSeqId, leftShift) {
 	glue.inMode("sequence/"+refSeqId, function() {
