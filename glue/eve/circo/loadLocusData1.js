f72470735ecacc9150a5fc0208e48c441d7bd9d0
Robert Gifford <robjgiff@gmail.com>
2020-11-24 12:36:15 +0000

    Refactoring

--- glue/eve/circo/loadLocusData1.js
+++ glue/eve/circo/loadLocusData1.js
@@ -1,5 +1,5 @@
 // list the circovirus EVE sequences
-var listSeqResult = glue.command(["list", "sequence", "-w", "source.name = 'fasta-curated-ecv'"]);
+var listSeqResult = glue.command(["list", "sequence", "-w", "source.name = 'fasta-digs-circo-ecv'"]);
 
 // extract from the result a list of sequence IDs.
 var seqIds = glue.getTableColumn(listSeqResult, "sequenceID");
@@ -11,7 +11,7 @@ _.each(seqIds, function(seqId) {
     glue.command(["create", "custom-table-row", "locus_data", seqId]);
     
     // associate the corresponding sequence with this object.
-    glue.inMode("sequence/fasta-curated-ecv/"+seqId, function() {
+    glue.inMode("sequence/fasta-digs-circo-ecv/"+seqId, function() {
         glue.command(["set", "link-target", "locus_data", "custom-table-row/locus_data/"+seqId]);    
     });
 
