8abd4ae6e744babec3775deb2caecd7994d05594
Rob Gifford <robjgiff@gmail.com>
2021-01-04 22:40:30 +0000

    Refactoring

---
+++ glue/eve/unc_circo/ecvUncCIrcoLoadLocusData2.js
@@ -0,0 +1,37 @@
+// Load EVE data from tab file 
+var loadResult;
+glue.inMode("module/cressTabularUtility", function() {
+	loadResult = glue.tableToObjects(glue.command(["load-tabular", "tabular/eve/ecv-circo-side-data.tsv"]));
+	// glue.log("INFO", "load result was:", loadResult);
+});
+
+_.each(loadResult, function(eveObj) {
+
+    // glue.log("INFO", "eveObj was:", eveObj);
+
+	glue.inMode("custom-table-row/locus_data/"+eveObj.sequenceID, function() {
+	
+		glue.command(["set", "field", "scaffold", eveObj.scaffold]);
+		glue.command(["set", "field", "start_position", eveObj.extract_start]);
+		glue.command(["set", "field", "end_position", eveObj.extract_end]);
+		glue.command(["set", "field", "orientation", eveObj.orientation]);
+		glue.command(["set", "field", "host_species", eveObj.organism]);
+		glue.command(["set", "field", "host_class", eveObj.host_class]);
+		glue.command(["set", "field", "host_superorder", eveObj.host_superorder]);
+		glue.command(["set", "field", "host_order", eveObj.host_order]);
+		glue.command(["set", "field", "host_family", eveObj.host_family]);
+
+
+	});
+
+	glue.inMode("sequence/fasta-digs-circo-ecv/"+eveObj.sequenceID, function() {
+	
+		glue.command(["set", "field", "name", eveObj.sequenceID]);
+		glue.command(["set", "field", "full_name", eveObj.sequenceID]);
+		glue.command(["set", "field", "family", "Circoviridae"]);
+		glue.command(["set", "field", "genus", "Circovirus"]);
+		glue.command(["set", "field", "clade", "NK"]);
+	});
+
+});
+
---
+++ glue/eve/unc_circo/ecvUncCircoLoadRefConData1.js
@@ -0,0 +1,21 @@
+// list the circovirus EVE sequences
+var listSeqResult = glue.command(["list", "sequence", "-w", "source.name = 'fasta-refseqs-circo-ecv'"]);
+
+// extract from the result a list of sequence IDs.
+var seqIds = glue.getTableColumn(listSeqResult, "sequenceID");
+
+// for each sequence ID
+_.each(seqIds, function(seqId) {
+
+	glue.log("INFO", "Sequence ID:", seqId);
+
+    // create an object in the custom table which uses the sequence ID as the row ID.
+    glue.command(["create", "custom-table-row", "eve_refcon_data", seqId]);
+    
+    // associate the corresponding sequence with this object.
+    glue.inMode("sequence/fasta-refseqs-circo-ecv/"+seqId, function() {
+        glue.command(["set", "link-target", "eve_refcon_data", "custom-table-row/eve_refcon_data/"+seqId]);    
+    });
+
+});
+
