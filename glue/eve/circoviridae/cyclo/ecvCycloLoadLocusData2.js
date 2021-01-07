2ea039deda5cdd14c195ef116faf8a96ecf35bfa
Rob Gifford <robjgiff@gmail.com>
2021-01-07 04:21:25 +0000

    Refactor

--- glue/eve/circoviridae/cyclo/ecvCycloLoadLocusData2.js
+++ glue/eve/circoviridae/cyclo/ecvCycloLoadLocusData2.js
@@ -1,13 +1,13 @@
 // Load EVE data from tab file 
 var loadResult;
 glue.inMode("module/cressTabularUtility", function() {
-	loadResult = glue.tableToObjects(glue.command(["load-tabular", "tabular/eve/ecv-cyclo-side-data.tsv"]));
-	// glue.log("INFO", "load result was:", loadResult);
+	loadResult = glue.tableToObjects(glue.command(["load-tabular", "tabular/eve/circoviridae/ecv-cyclo-side-data.tsv"]));
+	glue.log("INFO", "load result was:", loadResult);
 });
 
 _.each(loadResult, function(eveObj) {
 
-    // glue.log("INFO", "eveObj was:", eveObj);
+    glue.log("INFO", "eveObj was:", eveObj);
 
 	glue.inMode("custom-table-row/locus_data/"+eveObj.sequenceID, function() {
 	
@@ -15,11 +15,11 @@ _.each(loadResult, function(eveObj) {
 		glue.command(["set", "field", "start_position", eveObj.extract_start]);
 		glue.command(["set", "field", "end_position", eveObj.extract_end]);
 		glue.command(["set", "field", "orientation", eveObj.orientation]);
-		glue.command(["set", "field", "host_species", eveObj.organism]);
-		glue.command(["set", "field", "host_class", eveObj.host_class]);
-		glue.command(["set", "field", "host_superorder", eveObj.host_superorder]);
-		glue.command(["set", "field", "host_order", eveObj.host_order]);
-		glue.command(["set", "field", "host_family", eveObj.host_family]);
+		//glue.command(["set", "field", "host_species", eveObj.organism]);
+		//glue.command(["set", "field", "host_class", eveObj.host_class]);
+		//glue.command(["set", "field", "host_superorder", eveObj.host_superorder]);
+		//glue.command(["set", "field", "host_order", eveObj.host_order]);
+		//glue.command(["set", "field", "host_family", eveObj.host_family]);
 
 
 	});
